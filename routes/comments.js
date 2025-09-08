e72c70cd50b3d7e7781a98ab594049a5fe0910f4
Ndiaye Mamel Alboury <mamelalboury@gmail.com>
2025-09-08 06:17:03 +0200

    Get / comments

--- routes/comments.js
+++ routes/comments.js
@@ -3,3 +3,19 @@ const router = express.Router();
 
 const comments = require("../data/comments");
 const error = require("../utilities/error");
+
+/////////////////////////// GET /comments  ///////////////////
+router.get("/", (req, res) => {
+  let results = comments;
+
+  if (req.query.userId) {
+    results = results.filter(c => c.userId == req.query.userId);
+  }
+  if (req.query.postId) {
+    results = results.filter(c => c.postId == req.query.postId);
+  }
+
+  res.json({ comments: results });
+});
+
+module.exports = router;
