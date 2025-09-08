8c68cd1bc607cfdbf6774471a892d4c5791551d1
Ndiaye Mamel Alboury <mamelalboury@gmail.com>
2025-09-08 06:14:04 +0200

    adding comments file

---
+++ routes/users.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const router = express.Router();
+
+const users = require("../data/users");
+const error = require("../utilities/error");
+
+router
+  .route("/")
+  .get((req, res) => {
+    const links = [
+      {
+        href: "users/:id",
+        rel: ":id",
+        type: "GET",
+      },
+    ];
+
+    res.json({ users, links });
+  })
+  .post((req, res, next) => {
+    if (req.body.name && req.body.username && req.body.email) {
+      if (users.find((u) => u.username == req.body.username)) {
+        next(error(409, "Username Already Taken"));
+      }
+
+      const user = {
+        id: users[users.length - 1].id + 1,
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+      };
+
+      users.push(user);
+      res.json(users[users.length - 1]);
+    } else next(error(400, "Insufficient Data"));
+  });
+
+router
+  .route("/:id")
+  .get((req, res, next) => {
+    const user = users.find((u) => u.id == req.params.id);
+
+    const links = [
+      {
+        href: `/${req.params.id}`,
+        rel: "",
+        type: "PATCH",
+      },
+      {
+        href: `/${req.params.id}`,
+        rel: "",
+        type: "DELETE",
+      },
+    ];
+
+    if (user) res.json({ user, links });
+    else next();
+  })
+  .patch((req, res, next) => {
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        for (const key in req.body) {
+          users[i][key] = req.body[key];
+        }
+        return true;
+      }
+    });
+
+    if (user) res.json(user);
+    else next();
+  })
+  .delete((req, res, next) => {
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        users.splice(i, 1);
+        return true;
+      }
+    });
+
+    if (user) res.json(user);
+    else next();
+  });
+
+
+  const posts = require("../data/posts");
+const comments = require("../data/comments");
+
+////////////////////// GET /api/users/:id/posts //////////////////
+router.get("/:id/posts", (req, res) => {
+  const results = posts.filter(p => p.userId == req.params.id);
+  res.json(results);
+});
+
+/////////////////// GET /users/:id/comments /////////////////////
+router.get("/:id/comments", (req, res) => {
+  let results = comments.filter(c => c.userId == req.params.id);
+  if (req.query.postId) {
+    results = results.filter(c => c.postId == req.query.postId);
+  }
+  res.json(results);
+});
+
+
+module.exports = router;
